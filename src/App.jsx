9bc11e92625960a0e0fbce8451f863b8cf9a0a4a
Tanvir <tanvir.info07@gmail.com>
2024-09-21 19:33:12 +0600

    project setup for todo app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen  flex justify-center items-center">
+        <Button>React fresh with shad-cn</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
